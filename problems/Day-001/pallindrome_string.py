563c02b896160b2ecfd1cb7dfdbec856dc49eac6
Akshay Patil <akshaaypatil32@gmail.com>
2025-11-22 00:33:17 +0530

    first commit

---
+++ problems/Day-001/pallindrome_string.py
@@ -0,0 +1,10 @@
+
+# Detail,Description
+# Problem Statement,"Write a function that takes a string as input, 
+# returns the reversed version of the string, and also checks if the original string is 
+# a palindrome (reads the same forwards and backward, ignoring case)."
+# Input,"A string, e.g., ""madam"""
+# Output,"A tuple: (reversed_string, is_palindrome_boolean)"
+# Example 1,"Input: ""Level"" → Output: ('level', True)"
+# Example 2,"Input: ""Python"" → Output: ('nohtyp', False)"
+
